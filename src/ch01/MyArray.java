aaf12b48895e8d153122a70e2238a0e0a4ffa8e4
seokwon-yang <namugocps@gmail.com>
2021-12-04 12:29:06 +0900

    자바 - 배열(Array) #1

---
+++ src/ch01/MyArray.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class MyArray {
+    int[] intArr; //int array
+    int count;    //개수
+
+    public int ARRAY_SIZE;
+    //public static final int ERROR_NUM = -99999999999;
+
+    public MyArray(){
+
+    }
+}
