90c01f92de2755327e936d21e1a8097216958d12
SEOKWON-YANG <namugocps@gmail.com>
2022-01-29 23:11:44 +0900

    자바 - 배열(Array) #2

--- src/ch01/MyArray.java
+++ src/ch01/MyArray.java
@@ -5,9 +5,19 @@ public class MyArray {
     int count;    //개수
 
     public int ARRAY_SIZE;
-    //public static final int ERROR_NUM = -99999999999;
+    public static final int ERROR_NUM = -999999999;
 
     public MyArray(){
+        count = 0;
+        ARRAY_SIZE = 10;
+        intArr = new int[ARRAY_SIZE];
+    }
 
+    public void addElement(int num){
+        if(count >= ARRAY_SIZE){
+            System.out.println("not enough memory");
+            return;
+        }
+        intArr[count++] = num;
     }
 }
