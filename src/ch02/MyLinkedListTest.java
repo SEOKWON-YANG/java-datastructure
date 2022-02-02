3f22c7849e38e8234a1e78312138d49bedd6f074
SEOKWON-YANG <namugocps@gmail.com>
2022-02-02 23:00:34 +0900

    자료구조 #4
    자료구조 #3
    
    - LinkedList 나머지 구현

---
+++ src/ch02/MyLinkedListTest.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class MyLinkedListTest {
+
+    public static void main(String[] args){
+
+        MyLinkedList list = new MyLinkedList();
+        list.addElement("A");
+        list.addElement("B");
+        list.addElement("C");
+        list.printAll();
+    }
+}
